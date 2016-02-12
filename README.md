c3186469cdf7e8a6342eda5fcceba60a46264a90
delimitry <dvalimov@gmail.com>
2016-02-12 23:50:42 +0400

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # octree_color_quantizer
+
 Octree color quantizer in Python
+
+Description:
+------------
+Octree Quantizer class for image color quantization
+
+
+License:
+--------
+Released under [The MIT License](https://github.com/delimitry/octree_color_quantizer/blob/master/LICENSE).
---
+++ main.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+#-*- coding: utf8 -*-
+
+from PIL import Image
+from color import Color
+from octree_quantizer import OctreeQuantizer
+
+
+def main():
+    image = Image.open('rainbow.png')
+    pixels = image.load()
+    width, height = image.size
+
+    octree = OctreeQuantizer()
+
+    # add colors to the octree  
+    for j in xrange(height):
+        for i in xrange(width):
+            octree.add_color(Color(*pixels[i, j]))
+
+    # 256 colors for 8 bits per pixel output image 
+    palette = octree.make_palette(256)
+
+    # create palette for 256 color max and save to file
+    palette_image = Image.new('RGB', (16, 16))
+    palette_pixels = palette_image.load()
+    for i, color in enumerate(palette):
+        palette_pixels[i % 16, i / 16] = (color.red, color.green, color.blue)
+    palette_image.save('rainbow_palette.png')
+
+    # save output image
+    out_image = Image.new('RGB', (width, height))
+    out_pixels = out_image.load()
+    for j in xrange(height):
+        for i in xrange(width):
+            index = octree.get_palette_index(Color(*pixels[i, j]))
+            color = palette[index]
+            out_pixels[i, j] = (color.red, color.green, color.blue)
+    out_image.save('rainbow_out.png')
+
+
+if __name__ == '__main__':
+    main()
